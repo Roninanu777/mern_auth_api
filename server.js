aabce4e804c923431710a6a6aedd97026e661a86
Roninanu777 <ronirajkamalpradhan1112@gmail.com>
2020-08-08 22:33:50 +0530

    added mongouri

--- server.js
+++ server.js
@@ -13,15 +13,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 //Connect to MongoDB
-var env = process.env.NODE_ENV || 'development';
-var MONGO_URI = '';
-if(env === 'development'){
-    MONGO_URI = process.env.MONGO_URI_DEV;
-}
-else{
-    MONGO_URI = process.env.MONGO_URI_PROD;
-}
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+mongoose.connect(MONGO_URI_PROD, { useNewUrlParser: true, useUnifiedTopology: true })
 .then( () => console.log('MongoDB connected...'))
 .catch(err => console.log(err));
 
